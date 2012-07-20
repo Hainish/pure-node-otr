2d0ca37c0c2bc1f9d90ec5b2568ca74886de9f1d
William Budington <bill.budington@gmail.com>
2012-07-20 19:10:39 -0400

    separating out xmpp and otr tests, adding flag (to xmpp or not to xmpp)

--- test/spec/unit/xmpp_connect.js
+++ test/spec/unit/xmpp_connect.js
@@ -4,28 +4,29 @@ var _ = require('underscore');
 var xmpp_config = require('../../xmpp_config');
 var Otr = require('../../../lib/node-otr-native');
 var dh_commit_base64_wrapper;
+var cl;
 
-describe("client", function() {
-  var cl = new xmpp.Client({
-    jid: xmpp_config.client.jid,
-    password: xmpp_config.client.password,
-    host: xmpp_config.client.host,
-    port: xmpp_config.client.port
-  });
-  
-  it('should go online', function(done){
-    cl.on('online', function(){
-      assert.ok(true, 'client went online');
-      done();
+if(config.xmpp_dev == true){
+  describe("client", function() {
+    cl = new xmpp.Client({
+      jid: xmpp_config.client.jid,
+      password: xmpp_config.client.password,
+      host: xmpp_config.client.host,
+      port: xmpp_config.client.port
     });
-    cl.on('error', function(e){
-      console.log(e);
-      assert.fail(true, 'there was an error connecting: '+e);
-      done();
+    
+    it('should go online', function(done){
+      cl.on('online', function(){
+        assert.ok(true, 'client went online');
+        done();
+      });
+      cl.on('error', function(e){
+        console.log(e);
+        assert.fail(true, 'there was an error connecting: '+e);
+        done();
+      });
     });
-  });
-  
-  describe('node-otr-native', function(){
+
     it('should receive dh upon sending a message to recipient', function(done){
       cl.send(new xmpp.Element('presence', { }).c('show').t('chat').up().c('status').t('node-xmpp otr test'));
       setTimeout(function(){
@@ -47,11 +48,15 @@ describe("client", function() {
         }
       });
     });
-
-    it('should parse dh commit', function(){
-      var otr = new Otr();
-      otr.dh_commit_parse(dh_commit_base64_wrapper);
-    });
+  });
+}
+  
+describe('node-otr-native', function(){
+  it('should parse dh commit', function(){
+    dh_commit_base64_wrapper = "?OTR:AAICAAAAxNyJFLZknz8c0F98r6MH3Dr2GrMCuvp0DqfU1tA8BALSaj12ksOEBm2RstSeZ/R7ZOgOJzsOjIGrA7ygn5UHLu1h4RG0koRa04WSQs5vFCC2V1S2RU7u/JAQlBY262fLJIGw2/EbuLY5twb92TKOd/cj6AIEQxMdCelaSHG0NcIZWJH61FyfNRWyVRNWtKG50z0BpPX9swO+18wVV1hnK9AGbWiEJ/elHcmq10Izov+1DD7+zFis36e2zx/filaeRLW1fi4AAAAgChOq6J9QmqzmV6oGhHep2/7PQUYrKwe8syPSM7jg4nE=.";
+    var otr = new Otr();
+    otr.dh_commit_parse(dh_commit_base64_wrapper);
+    console.log(otr.dh_key_gen());
   });
 });
  
