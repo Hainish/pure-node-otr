ac99949194d4eb6039931a3d48917a5725458ce5
William Budington <bill.budington@gmail.com>
2012-07-20 20:32:35 -0400

    separating ake tests out into phases

--- test/spec/unit/xmpp_connect.js
+++ test/spec/unit/xmpp_connect.js
@@ -53,23 +53,28 @@ if(xmpp_config.xmpp_dev == true){
 }
   
 describe('node-otr-native', function(){
+  describe('AKE Phases 1 and 2 as Alice', function(){
+    before(function(){
+    if(xmpp_config.xmpp_dev != true) dh_commit_base64_wrapper = "?OTR:AAICAAAAxNyJFLZknz8c0F98r6MH3Dr2GrMCuvp0DqfU1tA8BALSaj12ksOEBm2RstSeZ/R7ZOgOJzsOjIGrA7ygn5UHLu1h4RG0koRa04WSQs5vFCC2V1S2RU7u/JAQlBY262fLJIGw2/EbuLY5twb92TKOd/cj6AIEQxMdCelaSHG0NcIZWJH61FyfNRWyVRNWtKG50z0BpPX9swO+18wVV1hnK9AGbWiEJ/elHcmq10Izov+1DD7+zFis36e2zx/filaeRLW1fi4AAAAgChOq6J9QmqzmV6oGhHep2/7PQUYrKwe8syPSM7jg4nE=.";
+      otr = new Otr();
+    });
 
-  before(function(){
-   if(xmpp_config.xmpp_dev != true) dh_commit_base64_wrapper = "?OTR:AAICAAAAxNyJFLZknz8c0F98r6MH3Dr2GrMCuvp0DqfU1tA8BALSaj12ksOEBm2RstSeZ/R7ZOgOJzsOjIGrA7ygn5UHLu1h4RG0koRa04WSQs5vFCC2V1S2RU7u/JAQlBY262fLJIGw2/EbuLY5twb92TKOd/cj6AIEQxMdCelaSHG0NcIZWJH61FyfNRWyVRNWtKG50z0BpPX9swO+18wVV1hnK9AGbWiEJ/elHcmq10Izov+1DD7+zFis36e2zx/filaeRLW1fi4AAAAgChOq6J9QmqzmV6oGhHep2/7PQUYrKwe8syPSM7jg4nE=.";
-    otr = new Otr();
-  });
+    it('should parse dh commit', function(){
+      otr.dh_commit_parse(dh_commit_base64_wrapper);
+      assert.equal(_.size(otr.dh_commit_hash), 6, "The parsed DH Commit Hash should have 6 components [MPI len is a separate component]");
+      assert(otr.dh_commit_hash['gxmpi_enc'].length >= 40, "The gxmpi length should be at least 320 bits");
+      assert.equal(otr.dh_commit_hash['gxmpi_hash'].length, 32, "The hash length should be 32 bytes");
+    });
 
-  it('should parse dh commit', function(){
-    otr.dh_commit_parse(dh_commit_base64_wrapper);
-    assert.equal(_.size(otr.dh_commit_hash), 6, "The parsed DH Commit Hash should have 6 components [MPI len is a separate component]");
-    assert(otr.dh_commit_hash['gxmpi_enc'].length >= 40, "The gxmpi length should be at least 320 bits");
-    assert.equal(otr.dh_commit_hash['gxmpi_hash'].length, 32, "The hash length should be 32 bytes");
-  });
+    it('should generate a dh key message gracefully', function(){
+      var dh_key = otr.dh_key_gen();
+      assert.equal(_.size(otr.dh_key_hash), 4, "The parsed DH Key Hash should have 4 components [MPI len is a separate component]");
+      assert.equal(otr.dh_key_hash['gy'].length, 192, "The DH g^y var should have a length of 192 bytes");
+    });
 
-  it('should generate a dh key message gracefully', function(){
-    var dh_key = otr.dh_key_gen();
-    assert.equal(_.size(otr.dh_key_hash), 4, "The parsed DH Key Hash should have 4 components [MPI len is a separate component]");
-    assert.equal(otr.dh_key_hash['gy'].length, 192, "The DH g^y var should have a length of 192 bytes");
+    after(function(){
+      if(xmpp_config.xmpp_dev == true) cl.send(new xmpp.Element('message', { to: xmpp_config.interlocutor, type: 'chat'}).c('body').t(dh_key));
+    });
   });
 });
  
